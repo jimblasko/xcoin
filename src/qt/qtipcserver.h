e86d122d22c096b74aa1eddcacdfdfb8ef581a30
el3ab <el3ab@gmail.com>
2015-02-27 23:39:41 +0200

    initial commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Xcoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "XcoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
